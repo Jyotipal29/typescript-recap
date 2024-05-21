9d292ad8d72faaefa8349c127a83694f0d0b38f3
jyoti pal <paljyoti0129@gmail.com>
2024-05-21 23:08:23 +0530

    learnt about types and interfaces

--- src/a.ts
+++ src/a.ts
@@ -33,13 +33,127 @@
 
 //4 create a function that takes another functions as input and runs it after 1 sec
 
-const fn1 = () => {
-  console.log(`hey jyoti`);
-};
-const run = (fn1: () => void) => {
-  setTimeout(() => {
-    fn1();
-  }, 1000);
-};
+// const fn1 = () => {
+//   console.log(`hey jyoti`);
+// };
+// const run = (fn1: () => void) => {
+//   setTimeout(() => {
+//     fn1();
+//   }, 1000);
+// };
 
-console.log(run(fn1));
+// console.log(run(fn1));
+
+// interfacees
+
+// to assign a type to the user object you can use interface
+
+//1 create a functio  isLegal that returns true or false if a user is above 18. it takes a user as an input
+// interface User {
+//   firstName: string,
+//   lastName:string
+//   age: number
+//   email?: string
+// }
+// let user = {
+//   firstName: "jyoti",
+//   lastName:"pal",
+//   age: 19
+// }
+
+
+
+// const isLegal = (user:User) => {
+//   if (user?.age > 18) {
+//     return true;
+//   }
+//   return false;
+
+// }
+// console.log(isLegal(user))
+/// only interface is alloowd to implement classes  not types
+
+
+
+// types
+// very similar to interface types let you aggregate data together
+
+type User  = {
+  name: string
+  email: string
+  age:number
+  
+}
+
+// types let you do few things
+//1 unions
+
+// type password = string | number
+
+
+//2 interface
+// What if you want to create a type that has every property of multiple types/ interfaces
+// type Employee = {
+//   name: string;
+//   startDate: Date;
+// };
+
+// type Manager = {
+//   name: string;
+//   department: string;
+// };
+
+// type TeamLead = Employee & Manager;
+
+// const teamLead: TeamLead = {
+//   name: "harkirat",
+//   startDate: new Date(),
+//   department: "Software developer",
+// };
+
+
+
+// arrays
+
+//1 Given an array of positive integers as input, return the maximum value in the array
+
+
+// let arr = [1, 2, 3, 4]
+
+// const maxVal = (arr: number[]) => {
+//   let max = 0;
+//   for (let i = 0; i < arr.length; i++){
+//     if(arr[i] > max)
+//     max = arr[i]
+//   }
+
+//   return max;
+// }
+
+
+// console.log(maxVal(arr))
+
+
+//enums
+
+//
+// enum Direction {
+//   Up,
+//   Down,
+//   Left,
+//   Right,
+// }
+
+// function doSomething(keyPressed: Direction) {
+//   // do something.
+// }
+
+// doSomething(Direction.Up);
+
+
+
+
+
+
+
+
